a6f899cff6befde057541a447c1c7262f7f208e1
Muller840 <vonmullerh@gmail.com>
2025-01-31 13:57:04 -0300

    server.js

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-const express = require('express');
 const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://<usuario>:<contraseña>@<tu-cluster>.mongodb.net/clientes', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const express = require('express');
+const app = express();
 const cors = require('cors');
 
-const app = express();
-const PORT = 5000;
-
-// Middleware
 app.use(cors());
 app.use(express.json());
-
-// Conectar a MongoDB
-mongoose.connect('mongodb://localhost:27017/clientes', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Esquema del cliente
 const clienteSchema = new mongoose.Schema({
@@ -33,6 +30,13 @@ app.post('/api/clientes', async (req, res) => {
     await cliente.save();
     res.status(201).send(cliente);
 });
+
+// Iniciar servidor
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
 
 // Iniciar servidor
 app.listen(PORT, () => {
